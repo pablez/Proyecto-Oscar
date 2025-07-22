fca433f61c37c4dfe7cfa4d99b5e8531b4b4b8d8
Oscar23102512 <Oscar23102512@gmail.com>
2025-07-21 21:24:24 -0400

    Hola

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo de Pablo");
@@ -7,6 +9,17 @@ public class Main {
         boolean estado = true;
         int cantidad;
         String descripcion;
+
+        //tienes que hacer datos de entrada y salida
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Cual es el precio del producto??");
+        precio = leer.nextInt();
+        System.out.println("En que estado se encuentra el producto?");
+        estado = leer.nextBoolean();
+        System.out.println("Cuantos productos hay?");
+        cantidad = leer.nextInt();
+
+
 
 
 
