f3652e82545d1002ab463d8c55ae1ba5a65c94a9
Oscar23102512 <Oscar23102512@gmail.com>
2025-07-21 21:46:29 -0400

    tarea

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,11 @@ public class Main {
         int cantidad;
         String descripcion;
 
+
+
+
+        
+
         //tienes que hacer datos de entrada y salida
         Scanner leer = new Scanner(System.in);
         System.out.println("Cual es el precio del producto??");
