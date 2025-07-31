a17b6c25c03b1a7182805075d67ddffa04cb3c48
pablez <pablogr2928@gmail.com>
2025-07-30 21:34:04 -0400

    registro de usuarios

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import javax.swing.plaf.IconUIResource;
+import java.io.FileWriter;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo de Pablo");
+    public static void main(String[] args) throws IOException {
 
         int precio = 0;
         String producto;
@@ -12,63 +13,73 @@ public class Main {
         double precio2 = 0.0; //10.5, 25.5, 13.5
         String descripcion;//abcABC123
 
-        Scanner leer = new Scanner(System.in);
-        /*
-
-        while(inicio <= limite)//1 <= 5
-        {
-            int numeroMisterioso = 12;
-            int numero;
-            System.out.println("Ingrese el numero");
-            numero = leer.nextInt();
-            if(numeroMisterioso == numero)//12 = 12
-            {
-                System.out.println("Exito encontraste el numero misterioso");
-                break;
-            }
-            else
-            {
-                inicio++;//1 2 3
-                System.out.println("No encontraste el numero misterios");
-            }
-            System.out.println("numero de intento N°: " + inicio);
-        }
-        */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // variable de sesion
         String usuario_Original = "oscargamer";
         String password_Original = "oscar111";
         String usuario;//pablo
         String password;
+
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su usuario");
+        usuario = leer.next();//pablo
+        System.out.println("Ingrese su contraseña");
+        password = leer.next();
+
+        // producto
+        // nombre y precio
+        // 3 datos
+        //
+        // se crea el archivo
+        String archivo = "usuario.txt";
+        // se crea nuestro ayudante
+        FileWriter escritor = new FileWriter(archivo, true);
+        // escribir o guardar los datos en el txt
+        escritor.write(usuario + "," + password + "\n");
+        // terminar de escribir
+        escritor.close();
+        // Muestra de exito
+        System.out.println("Usuario se registro exitosamente");
+
+        if (usuario_Original.equals(usuario) && password_Original.equals(password))//OscarGamer = pablo
+        {
+            System.out.println("Sesion iniciada");
+        }
+        else
+        {
+            System.out.println("Valio chetos ERROR AL INICIAR SESION");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         int oportunidad = 5;
 
@@ -81,27 +92,8 @@ public class Main {
         //2. inicio y 5  ++
         //3. oportunidad y 1 --
 
-        // ejercicio 3. 100
-        // 100 es par
-        // 99  es impar
-        // 98 es par
-        // ...
 
-
-        int galletas = 100;
-        while (galletas >= 1)
-        {
-            //usuario && contraseña
-            if(galletas%2 == 0)
-            {
-                System.out.println(galletas + " Es par");
-            }else
-            {
-                System.out.println(galletas+" Es impar");
-            }
-            galletas--;
-        }
-        // investigar como guardar datos en un txt en java
+        // investigar como guardar datos en un txt excel csv en java
 
 
 
@@ -125,43 +117,10 @@ public class Main {
 
 
 
-        System.out.println("Ingrese su usuario");
-        usuario = leer.next().toLowerCase();//pablo
-        System.out.println("Ingrese su contraseña");
-        password = leer.next().toLowerCase();
-        /**
-         * ==
-         * !=
-         * >
-         * <
-         * >=
-         * <=
-         *
-         */
-        if (usuario_Original.equals(usuario) && password_Original.equals(password))//OscarGamer = pablo
-        {
-            System.out.println("Sesion iniciada");
-        }
-        else
-        {
-            System.out.println("Valio chetos ERROR AL INICIAR SESION");
-        }
-        // aumentar al datos de entrada OSCARGamer igual al oscargamer
-        // aplicar el tolowercase en lodos los datos de entrada
 
 
-        System.out.println("Mi usuario es: "+ usuario + " Mi contraseña es: "+password);
-        //Seguridad
-        System.out.println("Como se llamara el producto? ");
-        producto = leer.next();
-        System.out.println("Cual es el precio del producto??");
-        precio = leer.nextInt();
-        System.out.println("En que estado se encuentra el producto?");
-        estado = leer.nextBoolean();
-        System.out.println("Cuantos productos hay?");
-        cantidad = leer.nextInt();
-        System.out.println("Descripcion del producto");
-        descripcion = leer.next();
+
+
 
 
 
