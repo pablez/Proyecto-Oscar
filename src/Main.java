350513828ba5176ac5b81ff1c4d7ccd7dabbbb93
Oscar23102512 <Oscar23102512@gmail.com>
2025-08-11 22:28:07 -0400

    cosas

--- src/Main.java
+++ src/Main.java
@@ -1,194 +1,49 @@
 import java.io.File;
-
-import javax.swing.plaf.IconUIResource;
 import java.io.FileWriter;
-import java.util.Scanner;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    private static String String;
-
     public static void main(String[] args) throws IOException {
-
-        // 2 variables lo pongas fuera cancha pero accesible para todos
-        String archivoUsuario = "usuario.txt";
-        String archivoProducto = "producto.txt";
         Scanner leer = new Scanner(System.in);
-        String nombre="";
-        String numero="";
-        String email="";
-        String password="";
+        System.out.println("Lista de personajes");
+        String personaje;
+        String arma;
+        String calificacion;
+        String archivosjuegos = "personajes.txt";
 
-        System.out.println("-----Menu Principal------");
-        System.out.println("1. Registrar nueva cuenta de usuario");
-        System.out.println("2. Inicio de sesion");
-        System.out.println("3. Salir");
-        System.out.println("Seleccione una opcion");
-        int opcion = leer.nextInt();
-        switch (opcion)
+
+        //Uso del While
+
+        int contador = 1;
+        String nombre;
+        while (contador <= 3)
         {
-            case 1:
-                System.out.println("----------Registro de Nuevos Usuarios---------");
-                System.out.println("Ingrese su nombre");
-                nombre = leer.next();
-                System.out.println("Ingres su numero de celular");
-                numero = leer.next();
-                System.out.println("Ingrese su usuario");
-                email = leer.next();
-                System.out.println("Ingrese su contraseña");
-                password = leer.next();
+            System.out.println("Como se llama tu personaje?");
 
-
-                FileWriter escritor = new FileWriter(archivoUsuario, true);
-                escritor.write(nombre + "," + numero + "," + email + "," + password + "\n");
+            personaje = leer.nextLine().trim();
+            System.out.println("Que arma usa tu personaje?");
+            arma = leer.nextLine().trim();
+            System.out.println("Que calificacion le das a tu personaje?");
+            calificacion = leer.nextLine().trim();
+            int califica = Integer.valueOf(calificacion);
+            if (califica > 0 && califica <= 10)
+            {
+                FileWriter escritor = new FileWriter(archivosjuegos,true);
+                escritor.write(personaje + " , " + arma + " , " + calificacion + "\n");
                 escritor.close();
-                System.out.println("Usuario se registro exitosamente");
 
-                break;
+            } else if (califica <= 0)
+            {
+                System.out.println("Odias a este personaje o que? 'pobrecit@' ");
+            } else if (califica > 10)
+            {
+                System.out.println("Solo podes poner la calificacion del 1 al 10, si quieres podes echarle la culpa al que me dijo que haga este limite :D ");
+            }
 
-            case 2:
-                System.out.println("-------Inicio de Sesion-------");
-                System.out.println("Ingresar su email");
-                email = leer.next();
-                System.out.println("Ingresar su contraseña");
-                password= leer.next();
-
-                //Scanner leer2 = new Scanner(System.in);
-
-                Scanner lectorArchivo = new Scanner(new File(archivoUsuario));//archivoUsuario.txt
-                if(!lectorArchivo.hasNextLine())//falso
-                {
-                    System.out.println("No hay usuarios en la base de datos");
-                }
-                while(lectorArchivo.hasNextLine())//verdad
-                {
-                    String linea = lectorArchivo.nextLine();//"Andres,7895412,andres@gmail.com,andres123"
-                    String[] datos = linea.split(",");//[Andres][7895412][andres@gmail.com][andres123]
-                                                    //array      [0]      [1]        [2]             [3]
-                                                    //length      1        2          3               4
-                    if(datos.length >= 4 && datos[2].equals(email) && datos[3].equals(password))
-                    {
-                        System.out.println("Inicio de sesion exitoso");
-                        System.out.println("------------Menu de Productos------------");
-                        System.out.println("1. Ver Produtos");
-                        System.out.println("2. Agregar Productos");
-                        System.out.println("3. Cerrar sesion");
-                        int opcion2 = leer.nextInt();
-                        switch (opcion2)
-                        {
-                            case 1:
-                                System.out.println("-------Lista de Productos------");
-                                File lectorArchivo2 = new  File(archivoProducto);
-                                if(!lectorArchivo2.exists())
-                                {
-                                    System.out.println("No hay productos para ver :(");
-                                }else{
-                                    File archivoProductos = new File(archivoProducto);
-                                    Scanner lectorArchivo3 = new Scanner(archivoProductos);
-                                    while(lectorArchivo3.hasNextLine())
-                                    {
-                                        String linea2 = lectorArchivo3.nextLine();
-                                        String[] datosProductos = linea.split(",");
-                                        if(datosProductos.length == 4)
-                                        {
-                                            System.out.println("Nombre: "+datosProductos[0]);
-                                            System.out.println("Precio: "+datosProductos[1]);
-                                            System.out.println("Cantidad: "+datosProductos[2]);
-                                            System.out.println("Descripción: "+datosProductos[3]);
-                                        }
-                                    }
-                                }
-
-                                break;
-                            case 2:
-                                //agregar o guardar los productos
-                                // nombre, precio, cantidad y descripcicon
-                                break;
-                            case 3:
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        System.out.println("Error de inicio de sesion");
-                    }
-                }
-                break;
-
-            case 3:
-                break;
-
-            default:
-                System.out.println("Opcion no es correcta, Ingresar datos del menu 1-3");
+            contador++;
         }
-
-        // tarea de CLase #9 es crear el menu y el switch de los productos y datos de entrada y salida y guardar datos al txt
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        int oportunidad = 5;
-
-        while(oportunidad >= 1)//0 >= 1
-        {
-            System.out.println("HOla");
-            oportunidad--;//5-4-3-2-1-0
-        }
-        //1. inicio y limite ++
-        //2. inicio y 5  ++
-        //3. oportunidad y 1 --
-
-
-        // investigar como guardar datos en un txt excel csv en java
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Gracias por poner tus personajes favoritos :D ");
 
 
 
